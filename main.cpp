1b3aa803c418d5d54fa8d453e1e53ee9f9d527a4
sodacris <glorykings@foxmail.com>
2021-10-30 13:21:41 +0800

    add qt version checking

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+    #if QT_VERSION_MAJOR < 6
+        QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    #endif
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,22 +1,30 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include<string>
-#include<iostream>
+#include <string>
+#include <iostream>
 using std::string;
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     std::string str = "Hello";
     QString qs = QString::fromStdString(str);
     ui->textEdit->append(qs);
-    ui->textEdit->append(qs);
+#if QT_VERSION_MAJOR < 6
+    ui->textEdit->append(
+        QString::fromStdString(
+            std::string(
+                "Qt version less than 6")));
+#else
+    ui->textEdit->append(
+        QString::fromStdString(
+            std::string(
+                "Qt version greater or equal 6")));
+#endif
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
